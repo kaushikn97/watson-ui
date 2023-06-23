b80b287de6b7bfd0149db3f5535f4734d4053b96
Harsh Nagda <harsh.nagda@goodera.com>
2023-06-23 20:53:51 +0530

    styling changes

---
+++ src/constants.js
@@ -0,0 +1,27 @@
+export const CHARACTERS = [
+	'plum',
+	'mustard',
+	'orchid',
+	'scarlett',
+	'green',
+	'peacock',
+];
+export const ROOMS = [
+	'kitchen',
+	'dining',
+	'hall',
+	'conservatory',
+	'study',
+	'billiards',
+	'library',
+	'lounge',
+];
+
+export const WEAPONS = [
+	'rope',
+	'pipe',
+	'revolver',
+	'dagger',
+	'candlestick',
+	'wrench',
+];
