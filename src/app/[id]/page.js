1666aeaab02b8bc3ae21d505386f92342fe9729b
Harsh Nagda <harsh.nagda@goodera.com>
2023-06-24 16:31:15 +0530

    UI complete hopefully

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -2,124 +2,191 @@
 
 import { useState, useEffect } from 'react';
 import Dropdown from 'react-dropdown';
-import Table from 'react-bootstrap/Table';
 import { CHARACTERS, WEAPONS, ROOMS } from '@/constants';
-import Multiselect from 'multiselect-react-dropdown';
+import { useRouter } from 'next/navigation';
 
 function CharactersTable({ data, players }) {
 	if (!data) return null;
 	return (
-		<Table>
-			<tbody>
-				{CHARACTERS.map((character) => (
-					<tr key={character}>
-						<td>{character}</td>
-						{players.map((player) => (
-							<td key={character + player}>
-								{data[player][character]}
-							</td>
-						))}
-					</tr>
-				))}
-			</tbody>
-		</Table>
+		<>
+			{CHARACTERS.map((character, idx) => (
+				<tr key={character}>
+					<th
+						className="sticky left-0 p-1 text-right bg-bg"
+						scope="row"
+					>
+						{character}
+					</th>
+					{players.map((player) => (
+						<td
+							key={character + player}
+							className={`p-1 text-center ${
+								idx % 2
+									? 'odd:bg-[#222] even:bg-[#111]'
+									: 'even:bg-[#222] odd:bg-[#111]'
+							}`}
+						>
+							{data[player][character]}
+						</td>
+					))}
+				</tr>
+			))}
+		</>
 	);
 }
 function RoomsTable({ data, players }) {
 	if (!data) return null;
 	return (
-		<Table>
-			<tbody>
-				{ROOMS.map((room) => (
-					<tr key={room}>
-						<td>{room}</td>
-						{players.map((player) => (
-							<td key={room + player}>{data[player][room]}</td>
-						))}
-					</tr>
-				))}
-			</tbody>
-		</Table>
+		<>
+			{ROOMS.map((room, idx) => (
+				<tr key={room}>
+					<th
+						className="sticky left-0 p-1 text-right bg-bg"
+						scope="row"
+					>
+						{room}
+					</th>
+					{players.map((player) => (
+						<td
+							key={room + player}
+							className={`p-1 text-center ${
+								idx % 2
+									? 'odd:bg-[#222] even:bg-[#111]'
+									: 'even:bg-[#222] odd:bg-[#111]'
+							}`}
+						>
+							{data[player][room]}
+						</td>
+					))}
+				</tr>
+			))}
+		</>
 	);
 }
 function WeaponsTable({ data, players }) {
 	if (!data) return null;
 	return (
-		<Table>
-			<tbody>
-				{WEAPONS.map((weapon) => (
-					<tr key={weapon}>
-						<td>{weapon}</td>
-						{players.map((player) => (
-							<td key={weapon + player}>
-								{data[player][weapon]}
-							</td>
-						))}
-					</tr>
-				))}
-			</tbody>
-		</Table>
+		<>
+			{WEAPONS.map((weapon, idx) => (
+				<tr key={weapon} className="">
+					<th
+						className="sticky left-0 p-1 text-right bg-bg"
+						scope="row"
+					>
+						{weapon}
+					</th>
+					{players.map((player) => (
+						<td
+							key={weapon + player}
+							className={`p-1 text-center ${
+								idx % 2
+									? 'odd:bg-[#222] even:bg-[#111]'
+									: 'even:bg-[#222] odd:bg-[#111]'
+							}`}
+						>
+							{data[player][weapon]}
+						</td>
+					))}
+				</tr>
+			))}
+		</>
 	);
 }
 
-function LogTable(data) {
-	// console.log(data.data);
-	if (!data) return null;
+function LogTable({ logs }) {
 	return (
-		<Table>
+		<table className="mx-auto">
+			<thead>
+				<tr>
+					{[
+						'Suggester',
+						'Character',
+						'Room',
+						'Weapon',
+						'Responder',
+						'Revealed card',
+					].map((val) => (
+						<th key={val} className="p-1 border border-border">
+							{val}
+						</th>
+					))}
+				</tr>
+			</thead>
 			<tbody>
-				{data.data.map((logEntry) => (
+				{logs.map((logEntry) => (
 					<tr key={logEntry.id}>
-						<td>{logEntry.suggester}</td>
-						<td>{logEntry.suggestion.character}</td>
-						<td>{logEntry.suggestion.room}</td>
-						<td>{logEntry.suggestion.weapon}</td>
-						<td>{logEntry.responder}</td>
-						<td>{logEntry.revealedCard}</td>
+						<td className="p-1 border border-border">
+							{logEntry.suggester}
+						</td>
+						<td className="p-1 border border-border">
+							{logEntry.suggestion.character}
+						</td>
+						<td className="p-1 border border-border">
+							{logEntry.suggestion.room}
+						</td>
+						<td className="p-1 border border-border">
+							{logEntry.suggestion.weapon}
+						</td>
+						<td className="p-1 border border-border">
+							{logEntry.responder}
+						</td>
+						<td className="p-1 border border-border">
+							{logEntry.revealedCard}
+						</td>
 					</tr>
 				))}
 			</tbody>
-		</Table>
+		</table>
 	);
 }
 export default function GamePage({ params: { id: sessionId } }) {
-	const [dialogOpen, setDialogOpen] = useState(true);
+	const router = useRouter();
+	const [dialogOpen, setDialogOpen] = useState(false);
 	const [players, setPlayers] = useState([]);
 	const [suggester, setSuggester] = useState('');
 	const [character, setCharacter] = useState('');
 	const [room, setRoom] = useState('');
 	const [weapon, setWeapon] = useState('');
 	const [responder, setResponder] = useState('');
-	const [responders, setResponders] = useState([]);
 	const [revealedCard, setRevealedCard] = useState('');
 	const [tableData, setTableData] = useState({});
 	const [log, setLog] = useState([]);
+
 	useEffect(() => {
-		fetch('http://localhost:8080/' + sessionId + '/playerList', {
+		fetch(`http://localhost:8080/${sessionId}/playerList`, {
 			method: 'GET',
 		})
 			.then((resp) => resp.json())
 			.then((list) => {
-				setPlayers(list);
-				// console.log(list);
+				if (list?.length) {
+					setPlayers(list);
+				} else {
+					throw new Error('old session');
+				}
+			})
+			.catch(() => {
+				localStorage.removeItem('sessionId');
+				router.push('/');
 			});
-		fetch('http://localhost:8080/' + sessionId + '/sourceOfTruth', {
-			method: 'GET',
-		})
+		fetch(`http://localhost:8080/${sessionId}/logs`)
+			.then((res) => res.json())
+			.then((list) => {
+				if (list?.length) {
+					setLog(list);
+				}
+			});
+	}, [sessionId, router]);
+
+	useEffect(() => {
+		fetch(`http://localhost:8080/${sessionId}/sourceOfTruth`)
 			.then((resp) => resp.json())
 			.then((data) => {
 				setTableData(data);
-				// console.log(data);
 			});
 	}, [sessionId, log]);
 
-	const selector = (setter, obj) => {
-		setter(obj.value);
-	};
-	console.log('responders:', responders);
-
 	const handleSubmit = () => {
-		fetch('http://localhost:8080/' + sessionId + '/addLogEntry', {
+		fetch(`http://localhost:8080/${sessionId}/addLogEntry`, {
 			method: 'POST',
 			body: JSON.stringify({
 				logEntry: {
@@ -137,27 +204,27 @@ export default function GamePage({ params: { id: sessionId } }) {
 			.then((resp) => resp.json())
 			.then((list) => {
 				setLog(list);
-				// console.log(list);
+				setDialogOpen(false);
 			});
 	};
 
 	return (
-		<main>
+		<main className="w-full overflow-auto">
 			<dialog
 				open={dialogOpen}
-				className="w-full py-0 text-white bg-transparent"
+				className="z-30 w-full h-full py-0 mt-10 text-white bg-transparent"
 			>
-				<section className="relative p-4 flex flex-col mx-auto items-center gap-4 max-w-2xl bg-[#222]/10 backdrop-blur-lg rounded-lg border border-[#fff]/50">
+				<section className="relative p-4 flex flex-col m-auto items-center gap-4 max-w-2xl bg-[#222]/80 backdrop-blur-lg rounded-lg border border-[#fff]/50">
 					<button
-						className="absolute top-2 right-4"
+						className="self-end"
 						onClick={() => setDialogOpen(false)}
 					>
 						&#10005;
 					</button>
-					<div className="">
+					<div className="w-full">
 						<Dropdown
 							options={players}
-							onChange={(obj) => selector(setSuggester, obj)}
+							onChange={({ value }) => setSuggester(value)}
 							value={suggester}
 							placeholder="Select player asking question"
 							className="relative w-full px-2 border rounded-md border-border"
@@ -165,68 +232,54 @@ export default function GamePage({ params: { id: sessionId } }) {
 							menuClassName="absolute top-7 bg-[#222] w-full border border-border rounded-md left-0 z-10 divide-y divide-[#aaa] [&>*]:px-2 py-1"
 						/>
 					</div>
-					<div className="flex flex-col items-center w-full gap-4 justify-evenly md:flex-row">
-						<div className="w-max">
-							<Dropdown
-								options={CHARACTERS}
-								onChange={(obj) => selector(setCharacter, obj)}
-								value={character}
-								placeholder="Select character"
-								className="relative w-full px-2 border rounded-md border-border"
-								placeholderClassName="text-[#aaa]"
-								menuClassName="absolute top-7 bg-[#222] w-full border border-border rounded-md left-0 z-10 divide-y divide-[#aaa] [&>*]:px-2 py-1"
-							/>
-						</div>
-						<div className="w-max">
-							<Dropdown
-								options={ROOMS}
-								onChange={(obj) => selector(setRoom, obj)}
-								value={room}
-								placeholder="Select room"
-								className="relative w-full px-2 border rounded-md border-border"
-								placeholderClassName="text-[#aaa]"
-								menuClassName="absolute top-7 bg-[#222] w-full border border-border rounded-md left-0 z-10 divide-y divide-[#aaa] [&>*]:px-2 py-1"
-							/>
-						</div>
-						<div className="w-max">
-							<Dropdown
-								options={WEAPONS}
-								onChange={(obj) => selector(setWeapon, obj)}
-								value={weapon}
-								placeholder="Select weapon"
-								className="relative w-full px-2 border rounded-md border-border"
-								placeholderClassName="text-[#aaa]"
-								menuClassName="absolute top-7 bg-[#222] w-full border border-border rounded-md left-0 z-10 divide-y divide-[#aaa] [&>*]:px-2 py-1"
-							/>
-						</div>
-					</div>
-					<div className="">
-						<Multiselect
-							options={players}
-							avoidHighlightFirstOption
-							selectedValues={responders}
-							placeholder="Select responder"
-							isObject={false}
-							onSelect={(val) => setResponders(val)}
-							onRemove={(val) => setResponders(val)}
-							style={{
-								chips: {
-									backgroundColor: '#222',
-									border: '1px solid white',
-								},
-								optionContainer: {
-									backgroundColor: '#111',
-								},
-								option: {
-									color: 'white',
-								},
-							}}
+					<div className="w-full">
+						<Dropdown
+							options={CHARACTERS}
+							onChange={({ value }) => setCharacter(value)}
+							value={character}
+							placeholder="Select character"
+							className="relative w-full px-2 border rounded-md border-border"
+							placeholderClassName="text-[#aaa]"
+							menuClassName="absolute top-7 bg-[#222] w-full border border-border rounded-md left-0 z-10 divide-y divide-[#aaa] [&>*]:px-2 py-1"
 						/>
 					</div>
-					<div className="">
+					<div className="w-full">
+						<Dropdown
+							options={ROOMS}
+							onChange={({ value }) => setRoom(value)}
+							value={room}
+							placeholder="Select room"
+							className="relative w-full px-2 border rounded-md border-border"
+							placeholderClassName="text-[#aaa]"
+							menuClassName="absolute top-7 bg-[#222] w-full border border-border rounded-md left-0 z-10 divide-y divide-[#aaa] [&>*]:px-2 py-1"
+						/>
+					</div>
+					<div className="w-full">
+						<Dropdown
+							options={WEAPONS}
+							onChange={({ value }) => setWeapon(value)}
+							value={weapon}
+							placeholder="Select weapon"
+							className="relative w-full px-2 border rounded-md border-border"
+							placeholderClassName="text-[#aaa]"
+							menuClassName="absolute top-7 bg-[#222] w-full border border-border rounded-md left-0 z-10 divide-y divide-[#aaa] [&>*]:px-2 py-1"
+						/>
+					</div>
+					<div className="w-full">
+						<Dropdown
+							options={players}
+							onChange={({ value }) => setResponder(value)}
+							value={responder}
+							placeholder="Select responder"
+							className="relative w-full px-2 border rounded-md border-border"
+							placeholderClassName="text-[#aaa]"
+							menuClassName="absolute top-7 bg-[#222] w-full border border-border rounded-md left-0 z-10 divide-y divide-[#aaa] [&>*]:px-2 py-1"
+						/>
+					</div>
+					<div className="w-full">
 						<Dropdown
 							options={[...CHARACTERS, ...ROOMS, ...WEAPONS]}
-							onChange={(obj) => selector(setRevealedCard, obj)}
+							onChange={({ value }) => setRevealedCard(value)}
 							value={revealedCard}
 							placeholder="Select revealed card"
 							className="relative w-full px-2 border rounded-md border-border"
@@ -242,19 +295,63 @@ export default function GamePage({ params: { id: sessionId } }) {
 					</button>
 				</section>
 			</dialog>
-			<button onClick={() => setDialogOpen(true)}>Enter data</button>
-
-			<section className="flex flex-col gap-10">
-				<CharactersTable
-					data={tableData.characterMap}
-					players={players}
-				/>
-				<RoomsTable data={tableData.roomMap} players={players} />
-				<WeaponsTable data={tableData.weaponMap} players={players} />
-			</section>
-
-			<section>
-				<LogTable data={log} />
+			<div className="flex justify-center mt-10">
+				<button
+					onClick={() => setDialogOpen(true)}
+					className="px-4 py-1 bg-white rounded-lg text-bg"
+				>
+					Enter data
+				</button>
+			</div>
+			<table className="relative mx-auto mt-10">
+				<thead className="sticky top-0">
+					<tr className="bg-bg">
+						<th className="bg-bg"></th>
+						{players.map((player) => (
+							<th key={player} className="px-3 py-1 bg-bg">
+								{player}
+							</th>
+						))}
+					</tr>
+				</thead>
+				<tbody className="">
+					<tr className="">
+						<th
+							className="sticky top-[31px] px-1 pt-4 pb-1 bg-bg"
+							colSpan={players?.length + 1}
+						>
+							Characters
+						</th>
+					</tr>
+					<CharactersTable
+						data={tableData.characterMap}
+						players={players}
+					/>
+					<tr className="">
+						<th
+							className="sticky top-[31px] px-1 pt-4 pb-1 bg-bg"
+							colSpan={players?.length + 1}
+						>
+							Rooms
+						</th>
+					</tr>
+					<RoomsTable data={tableData.roomMap} players={players} />
+					<tr className="">
+						<th
+							className="sticky top-[31px] px-1 pt-4 pb-1 bg-bg"
+							colSpan={players?.length + 1}
+						>
+							Weapons
+						</th>
+					</tr>
+					<WeaponsTable
+						data={tableData.weaponMap}
+						players={players}
+					/>
+				</tbody>
+			</table>
+			<section className="mx-auto mt-10">
+				<LogTable logs={log} />
 			</section>
 		</main>
 	);
